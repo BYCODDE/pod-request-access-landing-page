413097e870d4fe98c02e0c39c8c0a0e3d3fda86b
BYCODDE <anaraliev217@gmail.com>
2024-03-29 20:05:55 +0400

    final commit!

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const mainButton = document.getElementById("main_button");
 const mainH3 = document.getElementById("main_h3");
 mainButton.addEventListener("click", function () {
   const email = mainInput.value.trim();
-  // FIXME: და 1440/900-ზეა
 
   function validateEmail(email) {
     const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
