71ddf24c3877e6437d5a8786a077f1b4ad5f85f4
BYCODDE <anaraliev217@gmail.com>
2024-03-29 11:32:49 +0400

    new commit!

--- script.js
+++ script.js
@@ -2,7 +2,17 @@ const mainInput = document.getElementById("email");
 const mainButton = document.getElementById("main_button");
 const mainH3 = document.getElementById("main_h3");
 mainButton.addEventListener("click", function () {
-  if (!mainInput.value.includes("@")) {
+  // FIXME: unda shevcvalo amis logica rac miweria baqarma rac chamigdo და 1440/900-ზეა
+
+  function validateEmail(email) {
+    const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
+    return regex.test(email);
+  }
+
+  console.log(validateEmail(mainInput.value));
+
+  if (!validateEmail(mainInput.value)) {
     mainH3.style.display = "block";
   } else {
     mainH3.style.display = "none";
