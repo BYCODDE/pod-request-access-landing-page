9a9cd98d34c0f96a25aa9e9ca2a831cf6f922010
BYCODDE <anaraliev217@gmail.com>
2024-03-29 11:37:26 +0400

    new commit!

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const mainInput = document.getElementById("email");
 const mainButton = document.getElementById("main_button");
 const mainH3 = document.getElementById("main_h3");
 mainButton.addEventListener("click", function () {
-  // FIXME: unda shevcvalo amis logica rac miweria baqarma rac chamigdo და 1440/900-ზეა
+  // FIXME: და 1440/900-ზეა
 
   function validateEmail(email) {
     const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
